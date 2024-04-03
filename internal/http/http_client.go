f02eee82eac5555eae0de9125f3616a1c2332a07
Leon Latsch <git@leonlatsch.dev>
2024-04-03 17:04:55 +0200

    format http client

--- internal/http/http_client.go
+++ internal/http/http_client.go
@@ -11,26 +11,55 @@ import (
 
 var httpClient = &http.Client{}
 
+func GET(url string, headers map[string]string) (string, error) {
+	respBody, err := request("GET", url, headers, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return respBody, nil
+}
+
+func PUT(url string, headers map[string]string, requestBody interface{}) (string, error) {
+	respBody, err := request("PUT", url, headers, requestBody)
+	if err != nil {
+		return "", err
+	}
+
+	return respBody, nil
+}
+
+func PATCH(url string, headers map[string]string, requestBody interface{}) (string, error) {
+	respBody, err := request("PATCH", url, headers, requestBody)
+	if err != nil {
+		return "", err
+	}
+
+	return respBody, nil
+}
+
+// Make a request util
+
 func request(method string, url string, headers map[string]string, body interface{}) (string, error) {
-    // Serialize req body
+	// Serialize req body
 	reqBody, err := serialization.ToJson(body)
 	if err != nil {
 		return "", err
 	}
 
-    // Create request
+	// Create request
 	req, err := http.NewRequest(method, url, bytes.NewBufferString(reqBody))
 	if err != nil {
 		return "", err
 	}
 
-    // Add headers
-    req.Header.Add("Content-Type", "application/json")
-    for key, value := range headers {
-        req.Header.Add(key, value)
-    } 
+	// Add headers
+	req.Header.Add("Content-Type", "application/json")
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
 
-    // Execute request
+	// Execute request
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		return "", err
@@ -38,44 +67,16 @@ func request(method string, url string, headers map[string]string, body interfac
 
 	defer resp.Body.Close()
 
-    // Read resp body
+	// Read resp body
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 
-    respOk := resp.StatusCode >= 200 && resp.StatusCode < 300
-    if !respOk {
-        return string(respBody), errors.New(resp.Status)
-    }
+	respOk := resp.StatusCode >= 200 && resp.StatusCode < 300
+	if !respOk {
+		return string(respBody), errors.New(resp.Status)
+	}
 
-    return string(respBody), nil
-}
-
-func GET(url string, headers map[string]string) (string, error) {
-    respBody, err := request("GET", url, headers, nil)
-    if err != nil {
-        return "", err
-    }
-
-    return respBody, nil
-}
-
-func PUT(url string, headers map[string]string, requestBody interface{}) (string, error) {
-    respBody, err := request("PUT", url, headers,  requestBody)
-    if err != nil {
-        return "", err
-    }
-
-    return respBody, nil
-}
-
-
-func PATCH(url string, headers map[string]string, requestBody interface{}) (string, error) {
-    respBody, err := request("PATCH", url, headers,  requestBody)
-    if err != nil {
-        return "", err
-    }
-
-    return respBody, nil
+	return string(respBody), nil
 }
