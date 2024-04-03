9415c18ecd59ef663cf2744a6dc4f363ade91247
Leon Latsch <git@leonlatsch.dev>
2024-04-03 13:12:28 +0200

    move main logic go godaddy_service

---
+++ internal/service/godaddy_service.go
@@ -0,0 +1,105 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/leonlatsch/go-resolve/internal/api"
+	"github.com/leonlatsch/go-resolve/internal/cron"
+	"github.com/leonlatsch/go-resolve/internal/models"
+)
+
+type GodaddyService struct {
+	Config models.Config
+}
+
+func (self *GodaddyService) PrintDomainDetail() {
+	domainDetail, err := api.GetDomainDetail()
+	if err != nil {
+		log.Println("Could not load domain detail for " + self.Config.Domain)
+		log.Fatalln(err)
+	}
+
+	log.Println(
+		fmt.Sprintf(
+			"Config valid. Running for domain %s maintained by %s %s (%s)",
+			domainDetail.Domain,
+			domainDetail.ContactAdmin.FirstName,
+			domainDetail.ContactAdmin.LastName,
+			domainDetail.ContactAdmin.Email,
+		),
+	)
+}
+
+func (self *GodaddyService) ObserveAndUpdateDns() {
+    ipChan := self.observePublicIp()
+    lastIp := ""
+
+    for {
+        ip := <- ipChan
+
+        if ip == lastIp {
+            continue
+        }
+
+        if err := self.onIpChanged(ip); err != nil {
+			log.Println("Successfully updated all records. Caching " + ip)
+			lastIp = ip
+        } else {
+            log.Println(err)
+        }
+    }
+}
+
+func (self *GodaddyService) onIpChanged(ip string) error {
+	log.Println("Ip changed: " + ip)
+	for _, host := range self.Config.Hosts {
+
+		existingRecords, err := api.GetRecords(host)
+		if err != nil {
+			return err
+		}
+
+		record := models.DnsRecord{
+			Data: ip,
+			Name: host,
+			Type: "A",
+		}
+
+		switch len(existingRecords) {
+		case 0:
+			if err := api.CreateRecord(record); err != nil {
+				return err
+			}
+		case 1:
+			if err := api.UpdateRecord(record); err != nil {
+				return err
+			}
+		default:
+			return errors.New("Error. Check DNS A records on " + host)
+		}
+	}
+	return nil
+}
+
+func (self *GodaddyService) observePublicIp() chan string {
+	ipChan := make(chan string)
+	interval, err := time.ParseDuration(self.Config.Interval)
+	if err != nil {
+		log.Println("Could not read retry interval from config. Using fallback 1h")
+		interval = time.Hour
+	}
+
+	log.Println("Checking for new ip every " + self.Config.Interval)
+
+	go cron.Repeat(interval, func() {
+		currentIp, err := api.GetPublicIpAddress()
+		if err == nil {
+			ipChan <- currentIp
+		}
+	})
+
+	return ipChan
+}
--- main.go
+++ main.go
@@ -1,102 +1,19 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/leonlatsch/go-resolve/internal/api"
 	"github.com/leonlatsch/go-resolve/internal/config"
-	"github.com/leonlatsch/go-resolve/internal/cron"
-	"github.com/leonlatsch/go-resolve/internal/models"
+	"github.com/leonlatsch/go-resolve/internal/service"
 )
 
 func main() {
 	log.Println("Starting Application")
 	config.LoadConfig()
 
-    domainDetail, err := api.GetDomainDetail()
-    if err != nil {
-        log.Println("Could not load domain detail for " + config.SharedConfig.Domain)
-        log.Fatalln(err)
-    }
+    godaddyService := service.GodaddyService{Config: config.SharedConfig}
+    godaddyService.PrintDomainDetail()
 
-    log.Println(
-        fmt.Sprintf(
-            "Config valid. Running for domain %s maintained by %s %s (%s)",
-            domainDetail.Domain,
-            domainDetail.ContactAdmin.FirstName,
-            domainDetail.ContactAdmin.LastName,
-            domainDetail.ContactAdmin.Email,
-        ),
-    )
-
-	ipChan := observePublicIp()
-	lastIp := ""
-
-	for {
-		ip := <-ipChan
-
-		if ip == lastIp {
-			continue
-		}
-
-		if err := onIpChanged(ip); err == nil {
-			log.Println("Successfully updated all records. Caching " + ip)
-			lastIp = ip
-		} else {
-			log.Println(err)
-		}
-	}
+    godaddyService.ObserveAndUpdateDns()
 }
 
-func onIpChanged(ip string) error {
-    log.Println("Ip changed: " + ip)
-	for _, host := range config.SharedConfig.Hosts {
-
-		existingRecords, err := api.GetRecords(host)
-		if err != nil {
-			return err
-		}
-
-		record := models.DnsRecord{
-			Data: ip,
-			Name: host,
-			Type: "A",
-		}
-
-		switch len(existingRecords) {
-		case 0:
-			if err := api.CreateRecord(record); err != nil {
-				return err
-			}
-		case 1:
-			if err := api.UpdateRecord(record); err != nil {
-				return err
-			}
-		default:
-			return errors.New("Error. Check DNS A records on " + host)
-		}
-	}
-	return nil
-}
-
-func observePublicIp() chan string {
-	ipChan := make(chan string)
-	interval, err := time.ParseDuration(config.SharedConfig.Interval)
-	if err != nil {
-		log.Println("Could not read retry interval from config. Using fallback 1h")
-		interval = time.Hour
-	}
-
-    log.Println("Checking for new ip every " + config.SharedConfig.Interval)
-	go cron.Repeat(interval, func() {
-		currentIp, err := api.GetPublicIpAddress()
-		if err == nil {
-			ipChan <- currentIp
-		}
-	})
-
-	return ipChan
-}
