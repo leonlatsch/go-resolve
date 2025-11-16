12c0734b9d65efe4ee413c27c77b1f967591dddd
Leon Latsch <git@leonlatsch.dev>
2025-11-16 15:49:10 +0100

    fix ip updater not caching ip

--- internal/service/ip_observer_service.go
+++ internal/service/ip_observer_service.go
@@ -38,6 +38,7 @@ func (self *IpObserverService) observePublicIp() chan string {
 		currentIp, err := self.IpApi.GetPublicIpAddress()
 		if err == nil && currentIp != self.LastIp {
 			ipChan <- currentIp
+			self.LastIp = currentIp
 		}
 	})
 
